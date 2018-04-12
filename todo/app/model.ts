63cd23b27c897c12e6e6ae03af7fc04be228c378
shicks <shicks255@yahoo.com>
2018-04-11 21:52:18 -0400

    more work on angular app

---
+++ todo/app/model.ts
@@ -0,0 +1,9 @@
+var model = {
+    user: "Steven",
+    items: [
+        {action: "Buy Flowers", done: false },
+        {action: "Get Shoes", done: false },
+        {action: "Collect Tickers", done: true },
+        {action: "Call Rini", done: false }
+    ]
+}
