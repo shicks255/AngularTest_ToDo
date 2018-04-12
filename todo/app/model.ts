65f0ec16e8838b6719eacd113a419e5ff54570a2
shicks <shicks255@yahoo.com>
2018-04-12 11:14:35 -0400

    more work on angular app

--- todo/app/model.ts
+++ todo/app/model.ts
@@ -1,9 +1,29 @@
-var model = {
-    user: "Steven",
-    items: [
-        {action: "Buy Flowers", done: false },
-        {action: "Get Shoes", done: false },
-        {action: "Collect Tickers", done: true },
-        {action: "Call Rini", done: false }
-    ]
+//A model class
+//Questions:
+//Can i have more than 1 modeled thing in here?
+
+export class Model{
+    user;
+    items;
+
+    constructor(){
+        this.user = "Steve";
+        this.items =
+            [
+                new ToDoItem("Buy flowers", false),
+                new ToDoItem("Get Shoes", false),
+                new ToDoItem("Collect Tickets", true),
+                new ToDoItem("Call Rini", false)
+            ]
+    }
+}
+export class ToDoItem{
+    action;
+    done;
+
+    constructor(action, done)
+    {
+        this.action = action;
+        this.done = done;
+    }
 }
